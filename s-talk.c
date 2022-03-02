8cf12934ea96dcaff4d98f72595d3d61ec0589f6
Pigeon <patrick123cool@gmail.com>
2022-03-01 19:22:11 -0800

    better check

--- s-talk.c
+++ s-talk.c
@@ -10,8 +10,8 @@
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
-
-
+#define PORT "3490"
+#define BACKLOG 10 
 
 void receiveString(void *socketID) {
 
@@ -34,23 +34,23 @@ int main(int argc, char * argv[]) {
     if (argc != 4) {
         exit(EXIT_FAILURE);
     }
+    // remote connection info
     const int myPort = atoi(argv[1]);
     const char * remoteName = argv[2];
     const int remotePort = atoi(argv[3]);
 
-    // int status;
+    // getaddrinfo
     struct addrinfo hints, *res;
     memset(&hints, 0, sizeof(hints));
-
-    // getaddrinfo
     hints.ai_family = AF_INET; // IPV4
     hints.ai_socktype = SOCK_DGRAM; // UDP
-    hints.ai_flags = AI_PASSIVE;
+    hints.ai_flags = AI_PASSIVE; // use this computer IP
+
     int rv;
-    if ((rv = getaddrinfo(remoteName, remotePort, &hints, &res)) != 0) {
+    if ((rv = getaddrinfo(NULL, PORT, &hints, &res)) != 0) {
         perror("getaddrinfo bad");
         exit(EXIT_FAILURE);
-    }
+    }    
 
     int sockfd;
     struct addrinfo * i;
@@ -58,32 +58,34 @@ int main(int argc, char * argv[]) {
     for(i = res; i != NULL; i = i->ai_next) {
         // create socket
         if ((sockfd = socket(i->ai_family, i->ai_socktype, i->ai_protocol)) == -1) {
-            perror("client socket error");
+            perror("server socket error");
             continue;
         }
-
         // bind 
         if (bind(sockfd, i->ai_addr, i->ai_addrlen) == -1) {
-            perror("client bind error");
+            close(sockfd);
+            perror("server bind error");
             continue;
         }
-
         // connect
         if (connect(sockfd, i->ai_addr, i->ai_addrlen) == -1) {
             close(sockfd);
             perror("client connect error");
             continue;
         }
-
         break;
     }
+
+    freeaddrinfo(res);
 
     if (i == NULL) {
         perror("client failed to connect");
         return 2;
     }
 
-    // free
-    // freeaddrinfo(res);
+    if (listen(sockfd, BACKLOG) == -1) {
+        perror("listen error");
+        exit(1);
+    }
 
 }   
